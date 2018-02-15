85c8ddec7ba3db2b4cd16da6d7a71bf14985f2dd
janmilosh <janmilosh@gmail.com>
2018-02-14 20:34:27 -0500

    All tests passing

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -4,5 +4,18 @@ class Squares
   end
 
   def square_of_sum
+    (1..@num).reduce { |sum, num| sum + num }**2
   end
+
+  def sum_of_squares
+    (1..@num).reduce { |sum, num| sum + num**2 }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
+
+module BookKeeping
+  VERSION = 4
 end
