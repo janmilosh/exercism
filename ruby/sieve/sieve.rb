5e8b001f438f1c9e311542b6867c16eaff7c307f
janmilosh <janmilosh@gmail.com>
2018-02-16 20:57:25 -0500

    Working recursive solution

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,9 +1,29 @@
 class Sieve
   def initialize(limit)
-    @limit = limit
+    @primes = [*2..limit]
+    @index = 0
   end
 
   def primes
-    primes = []
+    return @primes if @primes.empty? || next_prime.nil?
+    remove_multiples_of(next_prime)
+    increment_next_prime_index
+    primes
   end
+
+  def next_prime
+    @primes[@index]
+  end
+
+  def increment_next_prime_index
+    @index += 1
+  end
+
+  def remove_multiples_of(num)
+    @primes.reject! { |n| n > num && (n % num).zero? }
+  end
+end
+
+module BookKeeping
+  VERSION = 1
 end
